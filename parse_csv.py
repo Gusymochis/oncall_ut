f71f2ce2fce954fa5733753571b11aa7a883842f
sumacherukuri11 <suma_cherukuri@intuit.com>
2014-01-27 22:17:56 +0000

    intial commit

---
+++ parse_csv.py
@@ -0,0 +1,12 @@
+import csv
+import os
+from datetime import date
+from datetime import datetime
+file_path = os.path.abspath('/home/pi/timetable.csv')
+f = open( file_path, 'rU' ) #open the file in read universal mode
+for line in f:
+       cells = line.split( "," )
+       date_object =  datetime.strptime(cells[0], '%m/%d/%Y')
+       if datetime.combine(date_object,datetime.min.time()) == datetime.combine(date.today(), datetime.min.time()):
+            print cells[ 0 ] #since we want the first, second and third column 
+
