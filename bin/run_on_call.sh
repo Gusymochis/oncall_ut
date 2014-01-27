5479006eec7847193eedfb9bce7548976330a5da
Tracy Fung <tracy_fung@intuit.com>
2014-01-27 14:53:20 -0800

    fix path to log in shell script.

--- bin/run_on_call.sh
+++ bin/run_on_call.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Starting script..." >>/home/pi/dev/log/on_call.log
-python /home/pi/dev/on_call.py >>/home/pi/dev/log/on_call.log
+echo "Starting script..." >>/home/pi/dev/on_call/log/on_call.log
+python /home/pi/dev/on_call.py >>/home/pi/dev/on_call/log/on_call.log
 
