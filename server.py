a12a6de15b06ef507b68cc2bb96d461a940f57ce
Tracy Fung <tracy_fung@intuit.com>
2014-01-28 15:21:07 -0800

    robot index.html

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import tornado.ioloop
 import tornado.web
 import new_parse_csv
+import tornado.template
+import os
+
+cwd = os.getcwd()
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write(new_parse_csv.get_oncall())
+    #self.write(parse_csv.get_oncall())
+    self.render("index.html", names=new_parse_csv.get_oncall())
 
 application = tornado.web.Application([
   (r"/", MainHandler),
+  (r"/(.*\.css)", tornado.web.StaticFileHandler, {"path": cwd}),
 ])
 
 if __name__ == "__main__":
