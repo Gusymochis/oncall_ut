87308b78de8d762be29410908181e7b267c7ca0b
Tracy Fung <tracy_fung@intuit.com>
2014-01-27 16:24:30 -0800

    no message

--- on_call_email.py
+++ on_call_email.py
@@ -1,13 +1,18 @@
 import smtplib
 from email.mime.text import MIMEText
 
-msg = MIMEText('get ready') 
+#email body
+msg = MIMEText('get ready')
+
+#put your from and to address here
 fromaddr = ''
 toaddr = ''
 msg['Subject'] = 'You are on call'
 msg['From'] = fromaddr 
 msg['To'] = toaddr
 
-s = smtplib.SMTP('')
+#put your smtp server here
+smtp_server = ''
+s = smtplib.SMTP(smtp_server)
 s.sendmail(fromaddr, [toaddr], msg.as_string())
 s.quit
