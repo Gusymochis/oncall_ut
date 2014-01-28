98c6e09c465b78840f43b5586f2d0e75f40fc325
Tracy Fung <tracy_fung@intuit.com>
2014-01-28 11:21:10 -0800

    added function on_call_email.lookup_and_send(‘name’) email_addr.csv to map name to email address properties.py is the dictionary to define properties.

---
+++ properties.py
@@ -0,0 +1,7 @@
+props = {}
+#props['email_addr_filename']="email_addr.csv"
+#props['smtp_server']=""
+#props['from_addr']=""
+
+if len(props) == 0:
+  print 'WARNING: Define properties in properties.py'
