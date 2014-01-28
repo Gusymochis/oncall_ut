94cfc62a72e2654ccf207912a1315d66678d1a19
Manuel Corona <manuel_corona-ayala@intuit.com>
2014-01-27 16:53:45 -0800

    function on parse.csv

--- parse_csv.py
+++ parse_csv.py
@@ -2,11 +2,14 @@ import csv
 import os
 from datetime import date
 from datetime import datetime
+
 file_path = os.path.relpath('./timetable.csv')
-f = open( file_path, 'rU' ) #open the file in read universal mode
-for line in f:
-       cells = line.split( "," )
-       date_object =  datetime.strptime(cells[0], '%m/%d/%Y')
-       if datetime.combine(date_object,datetime.min.time()) == datetime.combine(date.today(), datetime.min.time()):
-            print cells[ 0 ] + " " + cells[1] + " " + cells[2] #since we want the first, second and third column 
+
+def get_oncall():
+  f = open( file_path, 'rU' ) #open the file in read universal mode
+  for line in f:
+    cells = line.split( "," )
+    date_object =  datetime.strptime(cells[0], '%m/%d/%Y')
+    if datetime.combine(date_object,datetime.min.time()) == datetime.combine(date.today(), datetime.min.time()):
+      return cells[ 0 ] + " " + cells[1] + " " + cells[2] #since we want the first, second and third column 
 
