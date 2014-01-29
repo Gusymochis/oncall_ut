45f399eb6ca93d0927580ddc048c9ae7757d7dd5
Manuel Corona <manuel_corona-ayala@intuit.com>
2014-01-28 16:37:49 -0800

    parse renamed

--- parse_csv.py
+++ parse_csv.py
@@ -1,23 +1,41 @@
 import csv
 import os
+import datetime
+#import espeak
 from datetime import date
 from datetime import datetime
+from datetime import timedelta
 
 attr={}
+
+def compute_monday():
+  today = date.today()
+  offset = (today.weekday() - 0) % 7
+  last_monday = today - timedelta(days=offset)
+  convertedDate =	datetime.combine(last_monday, datetime.min.time())
+  return convertedDate
+
 def get_oncall():
   global attr
   f = open( 'timetable.csv', 'rU' ) #open the file in read universal mode
-  p = open( 'picture_mapping.csv', 'r')
+  p = open( 'pictures.csv', 'r')
   picture_path = {}
   for line in p:
-    name,path=line.split(',')
+    name,path=line.split('\n')[0].split(',')
     picture_path[name]=path
   for line in f:
-    cell = line.split( "," )
-    date_object =  datetime.strptime(cells[0], '%m/%d/%Y')
-    if datetime.combine(date_object,datetime.min.time()) == datetime.combine(date.today(), datetime.min.time()):
-      attr['date'] = cell[0]
-      atrr['user1'] = [cell[1],picture_path[cell[1]]
-      attr['user2'] = [cell[2],picture_path[cell[2]]
+    oc_date,name1,name2 = line.split('\n')[0].split(',')
+    dt =  datetime.strptime(oc_date, '%m/%d/%Y')
+    month,day,year = (int(x) for x in oc_date.split('/'))
+    oldDate = date(year,month,day)
+    dayofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    date_object =  datetime.strptime(oc_date, '%m/%d/%Y')
+   
+    if datetime.combine(date_object,datetime.min.time()) == compute_monday():
+      attr['date'] = oc_date
+      attr['user1'] = [name1,picture_path[name1]]
+      attr['user2'] = [name2,picture_path[name2]]
 
   return attr
+ 
+  
