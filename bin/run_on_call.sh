23f3e2c894cf69a59ee168c854d21bf0601581d2
Tracy Fung <tracy_fung@intuit.com>
2014-01-29 10:31:33 -0800

    call email test

--- bin/run_on_call.sh
+++ bin/run_on_call.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting script..." >>/home/pi/dev/on_call/log/on_call.log
-python /home/pi/ut_repo/oncall_ut/parse_csv.py >>/home/pi/dev/on_call/log/on_call.log
+python /home/pi/ut_repo/oncall_ut/email_test.py >>/home/pi/dev/on_call/log/on_call.log
 
