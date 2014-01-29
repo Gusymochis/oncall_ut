652d1783d2c00bd167107f2e86c25839dc19f4b7
Tracy Fung <tracy_fung@intuit.com>
2014-01-28 20:53:45 -0800

    fix image links

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ cwd = os.getcwd()
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
     attrib = parse.get_oncall()
-    print attrib['user1'][1]
+    print "date=%s" % attrib['date']
+    print "img=%s" % attrib['user1'][1]
+    print "img=%s" % attrib['user2'][1]
+
     #v_img1=attrib['user1'][1]
     #v_img2=attrib['user2'][1]
     self.render("index.html", data=attrib)
@@ -17,7 +20,7 @@ class MainHandler(tornado.web.RequestHandler):
 application = tornado.web.Application([
   (r"/", MainHandler),
   (r"/(.*\.css)", tornado.web.StaticFileHandler, {"path": cwd}),
-  (r"/(.*\.png)", tornado.web.StaticFileHandler, {"path": cwd}),
+  (r"/attr/pictures/(.*)", tornado.web.StaticFileHandler, {"path": "./attr/pictures"}),
 ])
 
 if __name__ == "__main__":
